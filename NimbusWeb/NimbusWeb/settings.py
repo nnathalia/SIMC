05145a705e7ec9bcfa8e68ca785bb936a92ebcc3
Nathalia Mariano Lopes <101136711+nnathalia@users.noreply.github.com>
2024-03-31 17:24:56 -0400

    add .env

--- NimbusWeb/NimbusWeb/settings.py
+++ NimbusWeb/NimbusWeb/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,11 +80,14 @@ WSGI_APPLICATION = 'NimbusWeb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NOME'),
+        'USER': os.getenv('DB_USUARIO'),
+        'PASSWORD': os.getenv('DB_SENHA'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORTA'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
