d10fb229f406160c12b86d1898afab06bb3950d4
Filipe Maciel <filipemaciellopes01@gmail.com>
2024-07-22 20:38:48 -0400

    Troca de nome para Flumen

--- FlumenWeb/FlumenWeb/urls.py
+++ FlumenWeb/FlumenWeb/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for NimbusWeb project.
+URL configuration for FlumenWeb project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from NimbusWeb import views 
+from FlumenWeb import views
 
 
 urlpatterns = [
