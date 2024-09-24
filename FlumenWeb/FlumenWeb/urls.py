b21928c219fdd32e908e03f4d05de19185e60952
Filipe Maciel <filipemaciellopes01@gmail.com>
2024-09-23 22:05:42 -0400

    add login

--- FlumenWeb/FlumenWeb/urls.py
+++ FlumenWeb/FlumenWeb/urls.py
@@ -1,20 +1,6 @@
-"""
-URL configuration for FlumenWeb project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from FlumenWeb import views
 
@@ -22,10 +8,12 @@ from FlumenWeb import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('login/', views.login, name='login'),
     path('cadastro/', views.cadastro, name='cadastro'),
     path('senha/', views.senha, name="senha"),
     path('dashboard/', views.dashboard, name="dashboard"),
-    path('gerador_relatorio/', views.relatorio, name="relatorio")
+    path('gerador_relatorio/', views.relatorio, name="relatorio"),
+
+    path('login/', LoginView.as_view(template_name='forms/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
 
--- FlumenWeb/FlumenWeb/views.py
+++ FlumenWeb/FlumenWeb/views.py
@@ -1,28 +1,27 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+
+@login_required
 def index(request):
     return render(request, 'index.html')
 
-def login(request):
-    return render(request, 'forms\login.html')
-    
+
+@login_required
 def cadastro(request):
     return render(request, 'forms\cadastro.html')
 
+
+@login_required
 def senha(request):
     return render(request, 'forms\senha.html')
 
+
+@login_required
 def dashboard(request):
     return render(request, 'pages\dashboard.html')
 
+
+@login_required
 def relatorio(request):
     return render(request, 'pages\gerador_relatorio.html')
-
-
-
-
-
-
-
-
-
