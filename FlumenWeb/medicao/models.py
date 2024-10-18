3f631e779e36cf9c9a18694ec7e5621edd30c95e
Filipe Maciel <filipemaciellopes01@gmail.com>
2024-10-17 22:21:16 -0400

    add models

---
+++ FlumenWeb/medicao/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from estacao.models import Estacao
+
+
+class Medicao(models.Model):
+    idMedicao = models.AutoField(primary_key=True)
+    data_hora = models.DateTimeField()
+    temperatura = models.FloatField()
+    umidade_ar = models.FloatField()
+    umidade_solo = models.FloatField()
+    luminosidade = models.FloatField()
+    chuva = models.FloatField()
+    qualidade_ar = models.FloatField()
+    idEstacao_fk = models.ForeignKey(Estacao, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+
+    def __str__(self):
+        return f"Medição {self.idMedicao} - {self.idEstacao_fk.nome_est}"
+
+class Meta:
+    db_table = 'flumen_medicao'
