467db6f287276104459fb45507841641ded0f845
nathalia <nmarianolopes@gmail.com>
2024-11-27 15:08:53 -0400

    update templates

--- FlumenWeb/FlumenWeb/views.py
+++ FlumenWeb/FlumenWeb/views.py
@@ -6,21 +6,19 @@ def index(request):
     return render(request, 'index.html')
 
 
-@login_required
 def cadastro(request):
     return render(request, 'forms\cadastro.html')
 
 
-@login_required
 def senha(request):
     return render(request, 'forms\senha.html')
 
 
-@login_required
+@login_required(redirect_field_name= 'login')
 def dashboard(request):
     return render(request, 'pages\dashboard.html')
 
 
-@login_required
+@login_required(redirect_field_name= 'login')
 def relatorio(request):
     return render(request, 'pages\gerador_relatorio.html')
