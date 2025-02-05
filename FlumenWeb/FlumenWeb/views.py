6714f190536801dc01d65e6c93621420a772acb6
nathalia <nmarianolopes@gmail.com>
2025-02-05 16:08:38 -0400

    update templates

--- FlumenWeb/FlumenWeb/views.py
+++ FlumenWeb/FlumenWeb/views.py
@@ -1,9 +1,40 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-
 def index(request):
-    return render(request, 'index.html')
+    colaboradores_context = [
+        {
+            "nome": "Nathalia Mariano",
+            "funcao": "Frontend",
+            "foto": "img/colaboradores/nathalia.jpeg",
+            "linkedin": "https://www.linkedin.com/in/nnathallia/"
+        },
+        {
+            "nome": "Filipe Maciel",
+            "funcao": "Backend",
+            "foto": "img/colaboradores/filipe.png",
+            "linkedin": "https://www.linkedin.com/in/nnathallia/"
+        },
+        # Adicione os demais colaboradores
+    ]
+
+    tecnologias_context = [
+        {"nome": "HTML", "link": "https://developer.mozilla.org/pt-BR/docs/Web/HTML", "img": "img/tecnologias/logo_html.svg"},
+        {"nome": "CSS", "link": "https://developer.mozilla.org/pt-BR/docs/Web/CSS", "img": "img/tecnologias/logo_css.svg"},
+        {"nome": "JavaScript", "link": "https://developer.mozilla.org/pt-BR/docs/Web/JavaScript", "img": "img/tecnologias/logo_javascript.svg"},
+        {"nome": "Python", "link": "https://www.python.org/", "img": "img/tecnologias/logo_python.svg"},
+        {"nome": "Django", "link": "https://www.djangoproject.com/", "img": "img/tecnologias/logo_django.svg"},
+        {"nome": "Bootstrap", "link": "https://getbootstrap.com/", "img": "img/tecnologias/logo_bootstrap.svg"},
+        {"nome": "PostgreSQL", "link": "https://www.postgresql.org/", "img": "img/tecnologias/logo_postgresql.svg"}
+    ]
+
+    
+    return render(request, 'index.html', {
+        "colaboradores_context": colaboradores_context,
+        "tecnologias_context": tecnologias_context,
+    })
+
+
     
 def cadastro(request):
     return render(request, 'auth\cadastro.html')
@@ -24,3 +55,8 @@ def dashboard(request):
 @login_required(redirect_field_name= 'login')
 def relatorio(request):
     return render(request, 'pages\gerador_relatorio.html')
+
+
+
+
+
