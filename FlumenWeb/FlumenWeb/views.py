0bdccba2f9c59423e8e5771727a131eed21795bd
FilipeMaciel <filipe@eficienciafiscal.com.br>
2025-03-24 17:19:46 -0400

    micro update -L

--- FlumenWeb/FlumenWeb/views.py
+++ FlumenWeb/FlumenWeb/views.py
@@ -37,24 +37,24 @@ def index(request):
 
     
 def cadastro(request):
-    return render(request, 'auth\cadastro.html')
+    return render(request, 'auth/cadastro.html')
 
 def email(request):
-    return render(request, 'auth\email.html')
+    return render(request, 'auth/email.html')
 
 
 def senha(request):
-    return render(request, 'auth\senha.html')
+    return render(request, 'auth/senha.html')
 
 
 @login_required(redirect_field_name= 'login')
 def dashboard(request):
-    return render(request, 'pages\dashboard.html')
+    return render(request, 'pages/dashboard.html')
 
 
 @login_required(redirect_field_name= 'login')
 def relatorio(request):
-    return render(request, 'pages\gerador_relatorio.html')
+    return render(request, 'pages/gerador_relatorio.html')
 
 
 
