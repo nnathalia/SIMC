7cfdacdca37ef7ff877c28d92e1b57ef0c8ca8bf
nathalia <nmarianolopes@gmail.com>
2025-09-24 21:35:51 -0400

    update

---
+++ SimcWeb/SimcWeb/data.py
@@ -0,0 +1,24 @@
+COLABORADORES = [
+  {
+    "nome": "Nathalia Mariano",
+    "funcao": "Frontend",
+    "foto": "img/colaboradores/nathalia.jpeg",
+    "linkedin": "https://www.linkedin.com/in/nnathallia/"
+  },
+  {
+    "nome": "Filipe Maciel",
+    "funcao": "Backend",
+    "foto": "img/colaboradores/filipe.png",
+    "linkedin": "https://www.linkedin.com/in/nnathallia/"
+  },
+  # Adicionar outros colaboradores
+]
+TECNOLOGIAS= [
+  {"nome": "HTML", "link": "https://developer.mozilla.org/pt-BR/docs/Web/HTML", "img": "img/tecnologias/logo_html.svg"},
+  {"nome": "CSS", "link": "https://developer.mozilla.org/pt-BR/docs/Web/CSS", "img": "img/tecnologias/logo_css.svg"},
+  {"nome": "JavaScript", "link": "https://developer.mozilla.org/pt-BR/docs/Web/JavaScript", "img": "img/tecnologias/logo_javascript.svg"},
+  {"nome": "Python", "link": "https://www.python.org/", "img": "img/tecnologias/logo_python.svg"},
+  {"nome": "Django", "link": "https://www.djangoproject.com/", "img": "img/tecnologias/logo_django.svg"},
+  {"nome": "Bootstrap", "link": "https://getbootstrap.com/", "img": "img/tecnologias/logo_bootstrap.svg"},
+  {"nome": "PostgreSQL", "link": "https://www.postgresql.org/", "img": "img/tecnologias/logo_postgresql.svg"}
+]
--- SimcWeb/SimcWeb/views.py
+++ SimcWeb/SimcWeb/views.py
@@ -9,39 +9,16 @@ from medicao.models import Medicao, Estacao
 from previsao.views import get_previsao
 from django.utils import timezone
 from django.db.models import Avg
+from .data import COLABORADORES, TECNOLOGIAS
 
 def index(request):
-    colaboradores_context = [
-        {
-            "nome": "Nathalia Mariano",
-            "funcao": "Frontend",
-            "foto": "img/colaboradores/nathalia.jpeg",
-            "linkedin": "https://www.linkedin.com/in/nnathallia/"
-        },
-        {
-            "nome": "Filipe Maciel",
-            "funcao": "Backend",
-            "foto": "img/colaboradores/filipe.png",
-            "linkedin": "https://www.linkedin.com/in/nnathallia/"
-        },
-        # Adicionar outros colaboradores
-    ]
 
-    tecnologias_context = [
-        {"nome": "HTML", "link": "https://developer.mozilla.org/pt-BR/docs/Web/HTML", "img": "img/tecnologias/logo_html.svg"},
-        {"nome": "CSS", "link": "https://developer.mozilla.org/pt-BR/docs/Web/CSS", "img": "img/tecnologias/logo_css.svg"},
-        {"nome": "JavaScript", "link": "https://developer.mozilla.org/pt-BR/docs/Web/JavaScript", "img": "img/tecnologias/logo_javascript.svg"},
-        {"nome": "Python", "link": "https://www.python.org/", "img": "img/tecnologias/logo_python.svg"},
-        {"nome": "Django", "link": "https://www.djangoproject.com/", "img": "img/tecnologias/logo_django.svg"},
-        {"nome": "Bootstrap", "link": "https://getbootstrap.com/", "img": "img/tecnologias/logo_bootstrap.svg"},
-        {"nome": "PostgreSQL", "link": "https://www.postgresql.org/", "img": "img/tecnologias/logo_postgresql.svg"}
-    ]
-
+    context = {
+        "colaboradores" : COLABORADORES,
+        "tecnologias" : TECNOLOGIAS
+    }
     
-    return render(request, 'index.html', {
-        "colaboradores_context": colaboradores_context,
-        "tecnologias_context": tecnologias_context,
-    })
+    return render(request, 'index.html', context)
 
 @login_required(redirect_field_name= 'login')
 def dashboard(request):
